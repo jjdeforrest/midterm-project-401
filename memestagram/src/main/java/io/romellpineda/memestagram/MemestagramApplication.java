cd7eba1b80f7148c51335b11c8a793978288da3e
Romell Pineda <romell.pineda@gmail.com>
2020-01-29 21:10:31 -0800

    first commit

---
+++ memestagram/src/main/java/io/romellpineda/memestagram/MemestagramApplication.java
@@ -0,0 +1,13 @@
+package io.romellpineda.memestagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemestagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemestagramApplication.class, args);
+	}
+
+}
---
+++ memestagram/src/test/java/io/romellpineda/memestagram/MemestagramApplicationTests.java
@@ -0,0 +1,13 @@
+package io.romellpineda.memestagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemestagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
