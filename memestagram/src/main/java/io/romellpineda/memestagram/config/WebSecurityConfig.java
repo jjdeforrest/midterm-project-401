b8c43965165bd65a2ba696d72fa45bc18733ec9e
jjdeforrest <jjdeforrest@users.noreply.github.com>
2020-02-04 11:36:33 -0800

    update

--- memestagram/src/main/java/io/romellpineda/memestagram/config/WebSecurityConfig.java
+++ memestagram/src/main/java/io/romellpineda/memestagram/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,6 +20,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     UserDetailServiceImpl userDeetS;
 
+
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDeetS).passwordEncoder(passwordEncoder());
     }
@@ -46,4 +49,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .permitAll();
     }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
 }
--- memestagram/src/main/java/io/romellpineda/memestagram/controllers/ApplicationUserController.java
+++ memestagram/src/main/java/io/romellpineda/memestagram/controllers/ApplicationUserController.java
@@ -3,6 +3,7 @@ package io.romellpineda.memestagram.controllers;
 import io.romellpineda.memestagram.models.ApplicationUser;
 import io.romellpineda.memestagram.models.ApplicationUserRepository;
 
+import io.romellpineda.memestagram.models.MemeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -22,10 +23,16 @@ import java.util.ArrayList;
 public class ApplicationUserController {
 
     @Autowired
-    ApplicationUserRepository applicationUserRepository;
+    public MemeRepository memeRepository;
+
+    @Autowired
+    public ApplicationUserRepository applicationUserRepository;
+
+
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
 
 
 
@@ -36,41 +43,52 @@ public class ApplicationUserController {
 
 
 
-    @PostMapping("/signup")
-    public RedirectView createNewApplicationUser(String username, String password, String profilePicture,
-                                                 String bio, String firstName){
-        ApplicationUser newUser = new ApplicationUser(username, passwordEncoder.encode(password), profilePicture, bio
-                , firstName);
-
+    @PostMapping("/join")
+    public RedirectView createNewApplicationUser(String username, String password, String profilePicture, String bio, String firstName){
+        ApplicationUser newUser = new ApplicationUser(username, passwordEncoder.encode(password), profilePicture, bio, firstName);
         applicationUserRepository.save(newUser);
 
         Authentication authentication = new UsernamePasswordAuthenticationToken(newUser, null, new ArrayList<>());
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        return new RedirectView("/profile");
+        return new RedirectView("/userprofile");
     }
 
 
 
     @GetMapping("/login")
     public String showLoginForm(){
+
         return "login";
     }
 
     @GetMapping("/users/{id}")
     public String showUserDetails(@PathVariable long id, Principal p, Model m){
         ApplicationUser usernameWeAreVisiting = applicationUserRepository.findById(id).get();
-
         m.addAttribute("usernameWeAreVisiting", usernameWeAreVisiting);
         m.addAttribute("principalName", p.getName());
-        return "public-view";
-    }
-
-    @GetMapping("/profile")
-    public String showMyProfile(Principal p, Model m){
-        ApplicationUser loggedInUser = applicationUserRepository.findByUsername(p.getName());
-
-        m.addAttribute("user", loggedInUser);
+        m.addAttribute("meme", memeRepository);
         return "profile";
     }
+
+    @GetMapping("/userprofile")
+    public RedirectView getLoggedInUsersId(Principal p, Model m){
+        if (p != null) {
+        ApplicationUser loggedInUser = applicationUserRepository.findByUsername(p.getName());
+        return new RedirectView("/users/" + loggedInUser.id);
+        } else {
+            return new RedirectView("/login");
+        }
+    }
+
+    @PostMapping("/logged")
+    public RedirectView authententicatedUser(Principal p, Model m) {
+        if (p != null) {
+            ApplicationUser loggedUser = applicationUserRepository.findByUsername(p.getName());
+            return new RedirectView("/users/" + loggedUser.getId());
+        } else {
+            return new RedirectView("/login");
+        }
+    }
+
 }
